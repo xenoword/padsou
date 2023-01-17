c0b9bef89558b8c3e3632d1138a9f05dcba5af14
maxpel-dev <maxime.peloutier@ymail.com>
2023-01-17 11:07:25 +0100

    Created text input component

--- app/src/main/java/com/example/padsou/ui/components/Input.kt
+++ app/src/main/java/com/example/padsou/ui/components/Input.kt
@@ -1,24 +1,55 @@
 package com.example.padsou.ui.components
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import com.example.padsou.ui.theme.Purple200
+import com.example.padsou.ui.theme.Shapes
 
 @Composable
-fun Input (name: String, onValueChange: (String) -> Unit, placeholder: String)
+fun Input (name: String, onValueChange: (String) -> Unit, placeholder: String,
+           lineCount: Int = 1, kbType: KeyboardType = KeyboardType.Text)
 {
-    TextField(
-        modifier = Modifier.fillMaxSize()
-            .border(width = 1.dp, color = Color(0xFF0000), RoundedCornerShape(20.dp)),
-        shape = RoundedCornerShape(1.dp),
-        value = name,
-        onValueChange = onValueChange,
-        placeholder = { Text(text = placeholder) },
-    )
+        var isSingleLine: Boolean = false
+        var kbTransformation: VisualTransformation = VisualTransformation.None
+
+        if (kbType == KeyboardType.Password) {
+                kbTransformation = PasswordVisualTransformation()
+        }
+        if (lineCount == 1) {
+                isSingleLine = true
+        }
+
+        Row(horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()) {
+                OutlinedTextField(
+                        visualTransformation = kbTransformation,
+                        singleLine = isSingleLine,
+                        keyboardOptions = KeyboardOptions(keyboardType = kbType),
+                        maxLines = lineCount,
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                                focusedBorderColor = Purple200,
+                                unfocusedBorderColor = Color.Transparent),
+                        shape = RoundedCornerShape(20.dp),
+                        textStyle = androidx.compose.ui.text.TextStyle.Default,
+                        modifier = Modifier
+                                //.clip(shape = RoundedCornerShape(10.dp))
+                                .height(((lineCount-1)*17).dp + 55.dp)
+                                .fillMaxWidth(0.85f),
+                                //.border(width = 1.dp, color = Color.Magenta, RoundedCornerShape(20.dp)),
+                        value = name,
+                        onValueChange = onValueChange,
+                        placeholder = { Text(text = placeholder) },
+                )
+        }
 }
