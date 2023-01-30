b80aacbdc536401b10e6aa33e7f68784d049d223
xenoword <mortier.moi91@gmail.com>
2023-01-30 09:18:51 +0100

    🚑  fix merge (pcq ct kc)

--- app/src/main/java/com/example/padsou/ui/pages/AddPlan/AddPlan.kt
+++ app/src/main/java/com/example/padsou/ui/pages/AddPlan/AddPlan.kt
@@ -1,6 +1,15 @@
 package com.example.padsou.ui.pages.AddPlan
 
+import android.content.ContentValues.TAG
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.graphics.Paint.Align
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.Orientation
@@ -15,6 +24,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -22,16 +33,39 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.padsou.models.Plan
 import com.example.padsou.ui.components.*
 import com.example.padsou.ui.theme.*
 import com.google.accompanist.pager.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
 
 @OptIn(ExperimentalPagerApi::class, ExperimentalSnapperApi::class)
 @Composable
 fun AddPlan(navController: NavHostController) {
+    // PLAN DATA
+    var currentPlan = Plan()
+    // IMAGE SELECTION STUFF ---------------
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+    val bitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
     var titleValue by remember { mutableStateOf("") }
     var descriptionValue by remember { mutableStateOf("") }
     var linkValue by remember { mutableStateOf("") }
@@ -112,9 +146,18 @@ fun AddPlan(navController: NavHostController) {
                                                 shape = RoundedCornerShape(20F),
                                                 onClick = {
                                                     scope.launch {
+                                                        currentPlan = Plan(
+                                                            title = titleValue,
+                                                            description = descriptionValue,
+                                                            link = linkValue
+                                                        )
                                                         pagerState.scrollToPage(1)
                                                     }
-                                                }
+                                                },
+                                            //Actif seulement quand les 3 champs sont renseignés
+                                            enabled = (!titleValue.isNullOrBlank() &&
+                                                    !descriptionValue.isNullOrBlank() &&
+                                                    !linkValue.isNullOrBlank())
                                         ) {
                                             Text("SUIVANT", fontWeight = FontWeight.Bold, fontSize = 16.sp, fontStyle = FontStyle.Normal,
                                                 fontFamily = Typography.h1.fontFamily, lineHeight = 21.sp, textAlign = TextAlign.Center, color = Color.White)
@@ -134,13 +177,37 @@ fun AddPlan(navController: NavHostController) {
 
                                         Divider(thickness = 20.dp, color = Color.Transparent)
                                         IconButton(
-                                                onClick = { /* TODO */ },
+                                                onClick = { launcher.launch("image/*") },
                                                 modifier = Modifier
                                                         .size(200.dp)
                                                         .background(MediumBlue, RoundedCornerShape(10.dp))) {
-                                            Icon(Icons.Rounded.Add, null,
+                                            imageUri?.let {
+                                                if (Build.VERSION.SDK_INT < 28) {
+                                                    bitmap.value = MediaStore.Images
+                                                        .Media.getBitmap(context.contentResolver, it)
+
+                                                } else {
+                                                    val source = ImageDecoder
+                                                        .createSource(context.contentResolver, it)
+                                                    bitmap.value = ImageDecoder.decodeBitmap(source)
+                                                }
+                                            }
+
+                                            bitmap.value?.let { btm ->
+                                                Image(
+                                                    bitmap = btm.asImageBitmap(),
+                                                    contentDescription = null,
+                                                    modifier = Modifier.fillMaxSize()
+                                                )
+                                            }
+
+                                            if (bitmap.value == null) {
+                                                Icon(
+                                                    Icons.Rounded.Add, null,
                                                     Modifier.fillMaxSize(0.4f),
-                                                    tint = LightGray)
+                                                    tint = LightGray
+                                                )
+                                            }
                                         }
 
                                         //Divider(thickness = 100.dp, color = Color.Transparent)
@@ -148,10 +215,34 @@ fun AddPlan(navController: NavHostController) {
                                                 .fillMaxSize()
                                                 .padding(bottom = 70.dp),
                                                 verticalAlignment = Alignment.Bottom) {
-                                            NavigateButton(text = "AJOUTER CE BON PLAN",
-                                                    backgroundcolor = MediumBlue,
-                                                    navController = navController,
-                                                    classDestination = "Home")
+                                            Button(colors = ButtonDefaults.buttonColors(backgroundColor = MediumBlue),
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                                    .padding(top = 0.dp, bottom = 10.dp)
+                                                    .height(50.dp),
+                                                shape = RoundedCornerShape(20F),
+                                                onClick = {
+                                                    scope.launch {
+                                                        // TODO
+                                                        uploadPlan(currentPlan, bitmap.value!!)
+                                                        navController.navigate("Home")
+                                                    }
+                                                },
+                                                //Actif seulement quand la photo est ajoutée
+                                                enabled = (bitmap.value != null)
+                                            )
+                                            {
+                                                Text(
+                                                    "AJOUTER CE BON PLAN",
+                                                    fontWeight = FontWeight.Bold,
+                                                    fontSize = 16.sp,
+                                                    fontStyle = FontStyle.Normal,
+                                                    fontFamily = Typography.h1.fontFamily,
+                                                    lineHeight = 21.sp,
+                                                    textAlign = TextAlign.Center,
+                                                    color = Color.White
+                                                )
+                                            }
                                         }
 
                                     }
@@ -169,4 +260,47 @@ fun AddPlan(navController: NavHostController) {
             }
         }
     }
+}
+
+private fun uploadPlan(plan: Plan, imgBitmap: Bitmap) {
+    // Create a storage reference from our app
+    val storageRef = Firebase.storage.reference
+
+    // Create a reference to "image.jpg"
+    val randName = (0..999999999).random().toString()
+    val imgRef = storageRef.child("$randName.jpg")
+
+    val imgImagesRef = storageRef.child("images/$randName.jpg")
+
+    val baos = ByteArrayOutputStream()
+    imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+    val data = baos.toByteArray()
+
+    var uploadTask = imgRef.putBytes(data)
+
+    val urlTask = uploadTask.addOnFailureListener { ex ->
+        //TODO HANDLE FAILURES
+        throw ex
+    }.addOnSuccessListener { task ->
+        task.storage.downloadUrl.addOnSuccessListener {
+            val downloadUri = it.toString()
+            plan.image = downloadUri
+            plan.nbTest = 0
+            plan.note = 0
+            plan.subTitle = ""
+            insertPlanInDatabase(plan)
+        }.addOnFailureListener{ ex ->
+            //TODO HANDLE FAILURES
+            throw ex
+        }
+    }
+}
+
+private fun insertPlanInDatabase(plan: Plan) {
+    val databaseRef = Firebase.firestore
+
+    databaseRef.collection("plans").document()
+        .set(plan.toFirebaseHashMap())
+        .addOnSuccessListener { Log.d(TAG, "Plan créé avec succès!")}
+        .addOnFailureListener { e -> Log.w(TAG, "Erreur lors de la création du plan !", e) }
 }
