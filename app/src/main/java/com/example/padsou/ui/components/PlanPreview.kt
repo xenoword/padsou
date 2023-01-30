f464a3f97cfc58229d4eddb2deec365c5470eeba
xenoword <mortier.moi91@gmail.com>
2023-01-30 15:23:39 +0100

    💄 fix preview

--- app/src/main/java/com/example/padsou/ui/components/PlanPreview.kt
+++ app/src/main/java/com/example/padsou/ui/components/PlanPreview.kt
@@ -11,32 +11,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.padsou.models.Plan
-import com.example.padsou.models.User
-import com.example.padsou.ui.pages.PlanDetail.PlanDetail
-import com.example.padsou.ui.theme.Teal200
-import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
-import com.google.firebase.ktx.Firebase
-import java.util.logging.Logger.global
 
 @Composable
-fun PlanPreview(plan: Plan, navController: NavHostController, height: Dp, width: Dp){
+fun PlanPreview(plan: Plan, navController: NavHostController, size: Dp){
 
     Column(
         Modifier
-            .height(height)
-            .width(width)
+            .size(size)
             .clickable {
                 navController.navigate("Detail/"+plan.id)
             }
@@ -74,10 +62,10 @@ fun PlanPreview(plan: Plan, navController: NavHostController, height: Dp, width:
             }
         }
 
-        Row() {
+        Row {
             Text(text = plan.title, fontSize = 12.sp, fontWeight = FontWeight.Bold)
         }
-        Row() {
+        Row {
             Text(text = plan.subTitle, fontSize = 8.sp, fontWeight = FontWeight(500))
         }
     }
